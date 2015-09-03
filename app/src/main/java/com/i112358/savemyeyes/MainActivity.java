ab15318c124bb38924c31c667cdc8562cd4f7b27
iv112358 <i112358@i112358s-MacBook-Pro.local>
2015-09-03 23:12:49 +0300

    we

--- app/src/main/java/com/i112358/savemyeyes/MainActivity.java
+++ app/src/main/java/com/i112358/savemyeyes/MainActivity.java
@@ -9,7 +9,6 @@ import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -19,38 +18,24 @@ import java.util.TimerTask;
 
 public class MainActivity extends Activity {
 
+    private static MainActivity activity;
     private int m_brightness = 255;
     private ContentResolver m_contentResolver;
-    private Window m_window;
     private Timer m_timer = null;
     private TimerTask m_timerTask = null;
-
-    private float m_fadeSeconds = 1.0f;
     Intent m_shakeService = null;
+
+    public static MainActivity Get() { return activity;}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.w("info", "MainActivity onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activity = this;
 
         m_shakeService = new Intent(this, ShakeService.class);
-
         m_contentResolver = getContentResolver();
-        m_window = getWindow();
-
-        try
-        {
-            m_brightness = android.provider.Settings.System.getInt(m_contentResolver, android.provider.Settings.System.SCREEN_BRIGHTNESS);
-        }
-        catch (Settings.SettingNotFoundException e)
-        {
-            e.printStackTrace();
-        }
-
-//        m_brightness = System.getInt(m_contentResolver, Settings.System.SCREEN_BRIGHTNESS);
-        Log.i("info", "Brightness is " + m_brightness);
-//        android.provider.Settings.System.putInt(m_contentResolver, android.provider.Settings.System.SCREEN_BRIGHTNESS, 100);
-
 
         RelativeLayout linearLayout = (RelativeLayout) findViewById(R.id.main_layout);
         TextView txt1 = new TextView(MainActivity.this);
@@ -65,43 +50,34 @@ public class MainActivity extends Activity {
     @Override
     public void onDestroy()
     {
+        Log.w("info", "MainActivity onDestroy");
         super.onDestroy();
         this.stopService(m_shakeService);
     }
 
     @Override
     public void onResume( ) {
+        Log.w("info", "MainActivity onResume");
         super.onResume();
 //        this.startService(m_shakeService);
         this.startService(new Intent(this, ShakeService.class));
 
         m_brightness = 0;
-
-        try {
-            m_timer = new Timer();
-            m_timerTask= new TimerTask() {
-                @Override
-                public void run() {
-                    m_brightness++;
-                    android.provider.Settings.System.putInt(m_contentResolver, android.provider.Settings.System.SCREEN_BRIGHTNESS, m_brightness);
-//                    Log.i("info", " new brightness is " + m_brightness);
-                    if ( m_brightness >= 255 ) {
-                        Log.i("info", "stop timer");
-                        m_timerTask.cancel();
-                    }
-                }
-            };
-            m_timer.schedule(m_timerTask, 50, (long)(1000 / 60) );
-        } catch ( Exception e ){
-
-        }
     }
 
     @Override
     public void onPause()
     {
-        Log.i("info", "MainActivity onPause");
+        Log.w("info", "MainActivity onPause");
         super.onPause();
+        this.stopService(new Intent(this, ShakeService.class));
+    }
+
+    @Override
+    public void onStop()
+    {
+        Log.w("info", "MainActivity onStop");
+        super.onStop();
         this.stopService(new Intent(this, ShakeService.class));
     }
 
@@ -125,5 +101,46 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void ChangeBrightness()
+    {
+        Log.i("info", "MainActivity ChangeBrightness");
+        try
+        {
+            m_brightness = android.provider.Settings.System.getInt(m_contentResolver, android.provider.Settings.System.SCREEN_BRIGHTNESS);
+        }
+        catch (Settings.SettingNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        Log.i("info", "Brightness is " + m_brightness);
+        final boolean directionUp = ( m_brightness <= 10 ) ? true : false;
+
+        try {
+            m_timer = new Timer();
+            m_timerTask= new TimerTask() {
+                @Override
+                public void run() {
+                    android.provider.Settings.System.putInt(m_contentResolver, android.provider.Settings.System.SCREEN_BRIGHTNESS, m_brightness);
+                    if ( directionUp ) {
+                        m_brightness++;
+                        if ( m_brightness >= 255 ) {
+                            Log.i("info", "stop timer");
+                            m_timerTask.cancel();
+                        }
+                    } else {
+                        m_brightness--;
+                        if ( m_brightness <= 0 ) {
+                            Log.i("info", "stop timer");
+                            m_timerTask.cancel();
+                        }
+                    }
+                }
+            };
+            m_timer.schedule(m_timerTask, 50, (long)(1000 / 60) );
+        } catch ( Exception e ) {
+
+        }
     }
 }
--- app/src/main/java/com/i112358/savemyeyes/ShakeService.java
+++ app/src/main/java/com/i112358/savemyeyes/ShakeService.java
@@ -8,12 +8,18 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.IBinder;
 import android.util.Log;
+import java.util.ArrayList;
 
 public class ShakeService extends Service implements SensorEventListener {
 
+    private final int ACCELERATION_ACCURACY = 10;
+    private final float ACCELERATION_STRENGTH = 15;
+    private final float ACCELERATION_TRIGGER = 12;
     private SensorManager m_sensorManager;
     private float m_lastAcceleration = SensorManager.GRAVITY_EARTH;
-    private float m_currentAcceleration = SensorManager.GRAVITY_EARTH;
+    private ArrayList<Float> m_accelerationList = new ArrayList<Float>();
+    private long m_lastChangeTime = -1;
+    private long m_timeOut = -1;
 
     @Override
     public IBinder onBind(Intent intent)
@@ -41,8 +47,7 @@ public class ShakeService extends Service implements SensorEventListener {
     {
         super.onStartCommand(intent, flags, startId);
         m_sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        m_sensorManager.registerListener(this, m_sensorManager
-                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        m_sensorManager.registerListener(this, m_sensorManager .getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_UI);
 
         return START_NOT_STICKY;
     }
@@ -58,18 +63,40 @@ public class ShakeService extends Service implements SensorEventListener {
         float x = event.values[0];
         float y = event.values[1];
         float z = event.values[2];
-        m_lastAcceleration = m_currentAcceleration;
 
-        m_currentAcceleration = (float) Math.sqrt((double) (x*x + y*y + z*z));
-        float delta = m_currentAcceleration - m_lastAcceleration;
-        m_currentAcceleration = m_currentAcceleration * 0.9f + m_lastAcceleration * 0.1f;
-        Log.i("info", " current shake is " + m_currentAcceleration);
+        float currentAcceleration = (float) Math.sqrt((double) (x*x + y*y + z*z));
+        currentAcceleration = currentAcceleration * 0.9f + m_lastAcceleration * 0.1f;
+        m_lastAcceleration = currentAcceleration;
+
+        if ( currentAcceleration > ACCELERATION_TRIGGER ) {
+            if ( m_lastChangeTime < 0 ) {
+                m_lastChangeTime = System.currentTimeMillis();
+            }
+        }
+
+        if ( m_lastChangeTime > 0 ) {
+            m_accelerationList.add(currentAcceleration);
+            if ( System.currentTimeMillis() - m_lastChangeTime > 1000 ) {
+                float avarege = 0.0f;
+                for ( Float elem : m_accelerationList ) {
+                    avarege += elem.floatValue();
+                }
+                avarege /= m_accelerationList.size();
+                Log.i("info", " avarege shake is " + avarege);
+                if ( avarege > ACCELERATION_STRENGTH ) {
+                    Log.i("info", "recorded " + m_accelerationList.size() + " amaunts");
+                    MainActivity.Get().ChangeBrightness();
+                }
+                m_lastChangeTime = -1;
+                m_accelerationList.clear();
+            }
+        }
+//        Log.i("info", " current shake is " + currentAcceleration);
     }
 
     protected void onResume()
     {
-        m_sensorManager.registerListener(this,m_sensorManager
-                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER),SensorManager.SENSOR_DELAY_NORMAL);
+        m_sensorManager.registerListener(this,m_sensorManager .getDefaultSensor(Sensor.TYPE_ACCELEROMETER),SensorManager.SENSOR_DELAY_UI);
     }
 
     protected void onPause()
